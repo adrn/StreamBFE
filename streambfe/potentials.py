3a55fe79983ea97263276f6007ff6cab4ea4258f
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-04-19 22:06:39 -0400

    define potentials

---
+++ streambfe/potentials.py
@@ -0,0 +1,16 @@
+__author__ = "adrn <adrn@astro.columbia.edu>"
+
+# Standard library
+from collections import OrderedDict as odict
+
+# Third-party
+import gary.potential as gp
+from gary.units import galactic
+
+potentials = odict()
+potentials['halo'] = gp.PlummerPotential(m=1E12, b=30, units=galactic)
+
+halo_disk = gp.CCompositePotential()
+halo_disk['disk'] = gp.MiyamotoNagaiPotential(m=6.E10, a=3, b=0.28, units=galactic)
+halo_disk['halo'] = gp.PlummerPotential(m=1E12, b=30, units=galactic)
+potentials['halo+disk'] = halo_disk
