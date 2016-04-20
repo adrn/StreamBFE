9f455988fbefc51bb1835f57b806fe1fc79ed985
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-04-20 10:24:44 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="streambfe",
+    version="0.1",
+    author="Adrian Price-Whelan",
+    author_email="adrn@astro.columbia.edu",
+    description="Fit streams with basis function expansion potentials.",
+    license="MIT",
+    packages=['streambfe'],
+    long_description="",
+)
